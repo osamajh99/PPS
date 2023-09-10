c7a45c98d96b7794e2a9133f3d53c79a801d0bdc
safaahaddad <35690797+safaahaddad@users.noreply.github.com>
2023-09-11 02:53:03 +0400

    Update User Info end point

--- Controller/User-ctrl.js
+++ Controller/User-ctrl.js
@@ -8,35 +8,52 @@ const JWT_SECRET = "scriptteam-secret-key";
 
 const SignUP = async (req, res) => {
      // Check if the req.body valid or not
-     const { Email, Password, UserName, PhoneNumber, Adress, IsAdmin } = req.body
+     const { Email, Password, UserName, PhoneNumber, Address, IsAdmin } = req.body
      const errors = SignUpValidation({ Email, Password })
 
-     if(errors) {
-         return res.status(400).json({ errors })
+     if (errors) {
+          return res.status(400).json({ errors })
      }
- 
+
      // Check if the Email is already exists 
      const oldUser = await User.findOne({ Email })
 
-     if(oldUser) {
-         return res.status(409).json({ error: 'User is already exists'})
+     if (oldUser) {
+          return res.status(409).json({ error: 'User is already exists' })
      }
-     bcrypt.hash(Password, saltRounds, async function(err, hash) {
-         // Store hash in your Password DB.
-         if(err) {
-             return res.status(403).json({ error: 'please try another passowrd'})
-         }
-         const newUSer = { Email , UserName, PhoneNumber, Adress, IsAdmin,  Password:  hash }
-         const dbUser = await User.create(newUSer)
-         const token = jwt.sign({
-          expiresIn: '1d',
-          data: dbUser
-           }, JWT_SECRET); 
-           return res.status(200).json({ token })
+     bcrypt.hash(Password, saltRounds, async function (err, hash) {
+          // Store hash in your Password DB.
+          if (err) {
+               return res.status(403).json({ error: 'please try another passowrd' })
+          }
+          const newUSer = { Email, UserName, PhoneNumber, Address, IsAdmin, Password: hash }
+          const dbUser = await User.create(newUSer)
+          const token = jwt.sign({
+               expiresIn: '1d',
+               data: dbUser
+          }, JWT_SECRET);
+          return res.status(200).json({ token })
      });
 
- }
- 
- module.exports = {
-    SignUP 
+}
+const UpdateUserInfo = async (req, res) => {
+     const { UserName, PhoneNumber, Address } = req.body
+     const userId = req.params.id
+     const filter = { _id: userId };
+     const update = { PhoneNumber: PhoneNumber, Address: Address, UserName: UserName };
+     const userObj = await User.findOneAndUpdate(filter, update)
+     if (!userObj) {
+          return res.status(400).json({ error: 'User Not Found' })
+     }
+     else {
+          return res.status(201).json({
+               success: true,
+               id: userId,
+               message: 'user Updated!',
+          })
+     }
+}
+module.exports = {
+     SignUP,
+     UpdateUserInfo
 }
--- Routes/User-router.js
+++ Routes/User-router.js
@@ -1,10 +1,12 @@
 const express = require('express')
 
 const UserCtrl = require('../Controller/User-ctrl')
+const { checkAuthUser } = require("../Middlewares/checkAuthUser");
 
  const router = express.Router()
 
  router.post('/signup',UserCtrl.SignUP)
+ router.post('/updateuserinfo/:id',[checkAuthUser],UserCtrl.UpdateUserInfo)
 
  module.exports = router
 
